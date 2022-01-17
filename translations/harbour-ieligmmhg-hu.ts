4478f8cf41c33fa66b24369d0fa99408e7304ead
1Zgp <36508793+1Zgp@users.noreply.github.com>
2022-01-17 12:29:30 +0200

    Create harbour-ieligmmhg-hu.ts

---
+++ translations/harbour-ieligmmhg-hu.ts
@@ -0,0 +1,221 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu_HU">
+<context>
+    <name>About</name>
+    <message>
+        <source>Senior
+This app address the demands of elderly jolla users. If you like to talk about this demands send me a mail: utkiek@public-files.de or lets drink a beer here in Bremen.</source>
+        <translation>Ez az alkalmazás az idősebb jolla felhasználók igényeit célozza. Ha szeretnél ezekről az igényekről beszélni, küldj egy e-mail-t az utkiek@public-files.de címre, vagy megihatunk egy sört itt Brémában.</translation>
+    </message>
+    <message>
+        <source>Ielig: mmHg
+Save your bloodpressure values</source>
+        <translation>Ielig: mmHg
+        Mentsd el a vérnyomás-értékeidet</translation>
+    </message>
+    <message>
+        <source>Privacy
+The values marked as private will not be exported.</source>
+        <translation>Adatvédelem
+        A privátként megjelölt adatok nem lesznek exportálva.</translation>
+    </message>
+    <message>
+        <source>Open source
+This app is QML and a litte C++.
+Github: https://github.com/Utkiek/IeligmmHg
+License: GPLv3</source>
+        <translation>Nyílt forrás
+        Az alkalmazás QML és egy kis C++ keveréke.
+        Github: https://github.com/Utkiek/harbour-ieligmmhg
+        Licenc: GPLv3</translation>
+    </message>
+    <message>
+        <source>Export
+You can export in a CSV-File. Path and filename is hardcoded to
+</source>
+        <translation>Exportálás
+        Exportálhatsz CSV fájlba. Az elérési útvonal és a fájlnév ide lesz kódolva</translation>
+    </message>
+    <message>
+        <source>/mmHg.csv</source>
+        <translation>/mmHg.csv</translation>
+    </message>
+</context>
+<context>
+    <name>Eingabe</name>
+    <message>
+        <source>Save</source>
+        <translation>Mentés</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Mégse</translation>
+    </message>
+    <message>
+        <source>Date</source>
+        <translation>Dátum</translation>
+    </message>
+    <message>
+        <source>Time</source>
+        <translation>Idő</translation>
+    </message>
+    <message>
+        <source>Systolic</source>
+        <translation>Szisztolés</translation>
+    </message>
+    <message>
+        <source>Diastolic</source>
+        <translation>Diasztolés</translation>
+    </message>
+    <message>
+        <source>Comment</source>
+        <translation>Megjegyzés</translation>
+    </message>
+    <message>
+        <source>Private</source>
+        <translation>Privát</translation>
+    </message>
+</context>
+<context>
+    <name>Einstellungen</name>
+    <message>
+        <source>Save</source>
+        <translation>Mentés</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Mégse</translation>
+    </message>
+    <message>
+        <source>Font size</source>
+        <translation>Betűméret</translation>
+    </message>
+    <message>
+        <source>small</source>
+        <translation>kicsi</translation>
+    </message>
+    <message>
+        <source>normal</source>
+        <translation>mormális</translation>
+    </message>
+    <message>
+        <source>big</source>
+        <translation>nagy</translation>
+    </message>
+    <message>
+        <source>About Ielig:mmHg</source>
+        <translation>A Ielig:mmHg-ről</translation>
+    </message>
+    <message>
+        <source>Delete all</source>
+        <translation>Az összes törlése</translation>
+    </message>
+</context>
+<context>
+    <name>Export</name>
+    <message>
+        <source>Create &apos;mmHg.csv&apos;</source>
+        <translation>&apos;mmHg.csv&apos; létrehozása</translation>
+    </message>
+    <message>
+        <source>date;</source>
+        <translation>dátum;</translation>
+    </message>
+    <message>
+        <source>time;</source>
+        <translation>idő;</translation>
+    </message>
+    <message>
+        <source>sys;</source>
+        <translation>sys;</translation>
+    </message>
+    <message>
+        <source>dia;</source>
+        <translation>dia;</translation>
+    </message>
+    <message>
+        <source>comment</source>
+        <translation>megjegyzés</translation>
+    </message>
+    <message>
+        <source>Path: </source>
+        <translation>Útvonal: </translation>
+    </message>
+    <message>
+        <source>... finished ...</source>
+        <translation>... befejezve ...</translation>
+    </message>
+    <message>
+        <source>Export</source>
+        <translation>Exportálás</translation>
+    </message>
+</context>
+<context>
+    <name>Kurve</name>
+    <message>
+        <source>values</source>
+        <translation>értékek</translation>
+    </message>
+</context>
+<context>
+    <name>NachfrageAllesLoeschen</name>
+    <message>
+        <source>Warning !</source>
+        <translation>Figyelem !</translation>
+    </message>
+    <message>
+        <source>Delete all values? Sure?</source>
+        <translation>Az összes érték törlése? Biztos?</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation>Nem</translation>
+    </message>
+    <message>
+        <source>Yes! Delete all values!</source>
+        <translation>Igen! Törölje az összes értéket!</translation>
+    </message>
+</context>
+<context>
+    <name>Wertliste</name>
+    <message>
+        <source>Chart</source>
+        <translation>Táblázat</translation>
+    </message>
+    <message>
+        <source>New value</source>
+        <translation>Új érték</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Szerkesztés</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Törlés</translation>
+    </message>
+    <message>
+        <source>Deleting</source>
+        <translation>Törlés</translation>
+    </message>
+    <message>
+        <source>values</source>
+        <translation>értékek</translation>
+    </message>
+    <message>
+        <source>Settings &amp; About</source>
+        <translation>Beállítások &amp; Névjegy</translation>
+    </message>
+    <message>
+        <source>Export</source>
+        <translation>Exportálás</translation>
+    </message>
+    <message>
+        <source>Add your data.
+For chart or export you need 2 (or more) values.</source>
+        <translation>Írd be az adataid.
+        Táblázathoz, vagy exportáláshoz 2, vagy több adatra van szükség.</translation>
+    </message>
+</context>
+</TS>
